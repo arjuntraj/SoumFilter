09eba15fb4df9101bee2d7f83e3b275c6436450a
Arjun T Raj <arjun441@gmail.com>
2022-04-22 22:38:57 +0400

    Adding checkbox to the filter view

--- src/component/ProductFilter.tsx
+++ src/component/ProductFilter.tsx
@@ -1,10 +1,12 @@
-import React  from 'react';
+import React from 'react';
 import {FlatList, View, Text} from 'react-native';
- 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 const TreeNode = ({nodeInfo}) => {
   return (
-    <View>
-      <Text>{nodeInfo.name}</Text>
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      <Icon name="checkbox-blank-outline" size={30} color="#000" />
+      <Text style={{flex: 1, marginTop: 7}}>{nodeInfo.name}</Text>
     </View>
   );
 };
@@ -17,6 +19,10 @@ const renderChildNode = ({children}) => {
 const renderFilterItem = ({item}) => {
   return (
     <View style={{paddingLeft: 20}}>
+      {/* <Icon name="checkbox-blank-outline" size={30} color="#000" />
+      <Icon name="checkbox-outline" size={30} color="#000" />
+      <Icon name="checkbox-multiple-outline" size={30} color="#000" /> */}
+
       <TreeNode nodeInfo={item} />
       {renderChildNode(item)}
     </View>
