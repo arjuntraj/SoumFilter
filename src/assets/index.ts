e8103a199f6339b61cf265345ea205a38a00e02f
Arjun T Raj <arjun441@gmail.com>
2022-04-23 11:43:45 +0400

    Adding images and assets

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+const soumLogo = require('./img/logo.png');
+const drawerWhite = require('./img/drawerWhite.png');
+const mobile = require('./img/product/mobile.png');
+
+
+export {soumLogo, drawerWhite,mobile};
