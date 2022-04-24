7274e862dbd979731a71f8b1d060f2cdcc92c07c
Arjun T Raj <arjun441@gmail.com>
2022-04-24 11:31:36 +0400

    Added tree view with checkbox and selection

--- src/component/ProductFilter.tsx
+++ src/component/ProductFilter.tsx
@@ -1,41 +1,128 @@
-import React from 'react';
-import {FlatList, View, Text} from 'react-native';
+import {cloneDeep} from 'lodash';
+import React, {useEffect, useState} from 'react';
+import {FlatList, View, Text, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {FilterProps, NodeComponentProps, TreeNode} from '../types';
+import {getNodeID} from '../util';
+ 
+const NodeComponent = ({
+  node,
+  onToggleNodeView,
+  toggleNodeSelection,
+}: NodeComponentProps) => {
+  console.log('render tree node');
 
-const TreeNode = ({nodeInfo}) => {
   return (
     <View style={{flex: 1, flexDirection: 'row'}}>
-      <Icon name="checkbox-blank-outline" size={30} color="#000" />
-      <Text style={{flex: 1, marginTop: 7}}>{nodeInfo.name}</Text>
+      <TouchableOpacity
+        onPress={() => {
+          toggleNodeSelection(node);
+        }}>
+        <Icon
+          name={node?.selected ? 'checkbox-outline' : 'checkbox-blank-outline'}
+          size={30}
+          color="#000"
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          onToggleNodeView(node);
+        }}>
+        <Text style={{flex: 1, marginTop: 7}}>{node?.name}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const renderChildNode = ({children}) => {
-  if (children) {
-    return children.map(node => renderFilterItem({item: node}));
-  }
-};
-const renderFilterItem = ({item}) => {
-  return (
-    <View style={{paddingLeft: 20}}>
-      {/* <Icon name="checkbox-blank-outline" size={30} color="#000" />
-      <Icon name="checkbox-outline" size={30} color="#000" />
-      <Icon name="checkbox-multiple-outline" size={30} color="#000" /> */}
-
-      <TreeNode nodeInfo={item} />
-      {renderChildNode(item)}
-    </View>
-  );
-};
-function ProductFilter(props) {
+function ProductFilter(props: FilterProps) {
   const {data} = props;
+  const [expandedNodes, setExpandedNodes] = useState<Array<string>>([]);
+  const [selecteddNodes, setSelecteddNodes] = useState<Array<string>>([]);
+  //TODO for selected list
+  const [expandedNodesObj, setExpandedNodesObj] = useState({});
+  const [flatListkey, setFlatListKey] = useState(Math.random());
+
+  useEffect(() => {
+    setFlatListKey(selecteddNodes.length * expandedNodes.length);
+  }, [selecteddNodes, expandedNodes]);
+
+  const selectChildNode = (node: TreeNode, currentState: boolean) => {
+    node.selected = !currentState;
+    node?.children?.map((child: TreeNode) =>
+      selectChildNode(child, currentState),
+    );
+  };
+
+  const shrinkChild = (node: TreeNode, currentState: boolean) => {
+    node.expanded = !currentState;
+    node?.children?.map((child: TreeNode) => shrinkChild(child, currentState));
+  };
+
+  const toggleNodeSelection = (node: TreeNode) => {
+    //optional selectiuon value
+    const currentSelection = !!node.selected;
+    //generate NodeId
+    const nodeId = getNodeID(node);
+    if (selecteddNodes.includes(nodeId)) {
+      node.selected = false;
+      setSelecteddNodes(selecteddNodes.filter(id => id !== nodeId));
+    } else {
+      node.selected = true;
+      setSelecteddNodes([...selecteddNodes, nodeId]);
+    }
+    selectChildNode(node, currentSelection);
+  };
+
+  const onToggleNodeView = (node: TreeNode) => {
+    const nodeId: string = getNodeID(node);
+    const isExpanded = node?.expanded;
+    if (expandedNodes.includes(nodeId)) {
+      // console.log(nodeId + 'includes in ' + expandedNodes);
+      node.expanded = false;
+      setExpandedNodes(expandedNodes.filter(item => item !== nodeId));
+    } else {
+      node.expanded = true;
+      setExpandedNodes([...expandedNodes, nodeId]);
+    }
+    if (isExpanded) {
+      // TODO
+      // shrinkChild(node, isExpanded);
+    }
+  };
+  const renderChildNode = (node: TreeNode) => {
+    const {children, parentNode} = node;
+    const isShowNode = node.expanded;
+    if (children && isShowNode) {
+      return children.map((childNode: TreeNode) => {
+        if (!childNode.parentNode) {
+          childNode.parentNode = node;
+        }
+        return renderFilterItem({item: childNode});
+      });
+    }
+  };
+  const renderFilterItem = ({item}: {item: TreeNode}) => {
+    const nodeId: string = getNodeID(item);
+
+    return (
+      <View style={{paddingLeft: 20}} key={item.id}>
+        <NodeComponent
+          node={item}
+          toggleNodeSelection={toggleNodeSelection}
+          onToggleNodeView={onToggleNodeView}
+        />
+        {renderChildNode(item)}
+      </View>
+    );
+  };
 
   return (
     <FlatList
       data={data}
       renderItem={renderFilterItem}
       showsVerticalScrollIndicator={true}
+      keyExtractor={(ele, pos) => pos.toString()}
+      extraData={flatListkey}
     />
   );
 }
