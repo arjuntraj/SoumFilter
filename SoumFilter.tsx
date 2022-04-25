a466117c323748388e9863058babd436de801e5a
Arjun T Raj <arjun441@gmail.com>
2022-04-25 18:23:38 +0400

    added selected option formatting

--- SoumFilter.tsx
+++ SoumFilter.tsx
@@ -1,20 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import ProductFilter from './src/component/ProductFilter';
 
 import {productCategory, productList} from './src/mock';
-import {NodeComponentProps} from './src/types';
+import {NodeComponentProps, TreeNode} from './src/types';
 import {getFIlterDataFromProductMetadata} from './src/util';
 
-
-
-/**  
- * 
- * View foe each Node 
+/**
+ *
+ * View foe each Node
  * node : Complete node object
- * 
+ *
  */
 const NodeComponent = ({
   node,
@@ -78,14 +76,94 @@ const App = () => {
   let copyOfProductList: any[] = JSON.parse(JSON.stringify(productList));
 
   let [filtereredList, setfFltereredList] = useState<any[]>(copyOfProductList);
+  let [selectedFilters, setSelectedFilters] = useState<Array<string>>([]);
+
   let [productCategoryData, setProductCategory] =
     useState<any[]>(copyOfCategoryList);
 
-  const filterMetadata = getFIlterDataFromProductMetadata(
-    productCategoryData,
-    filtereredList,
-  );
+  useEffect(() => {
+    //TODO
+  }, [selectedFilters]);
 
+  const formatFilterSelection = (list: TreeNode[]) => {
+    //TODO: Cleanup implemetation
+    const selectedFilterOptions = [];
+    const selectedList = Object.keys(list).map(function (key) {
+      return list[key];
+    });
+
+    // grouping by type
+    let filterByType = selectedList.reduce(
+      (filterList, item) => ({
+        ...filterList,
+        [item.type]: [...(filterList[item.type] || []), item],
+      }),
+      {},
+    );
+    const {category = [], brand = [], model = [], varient = []} = filterByType;
+
+    // get selected category list
+    const selectedcategory = category.map((o: TreeNode) => 'All ' + o.name);
+
+    selectedcategory.length && selectedFilterOptions.push(selectedcategory);
+
+    // get selected brand list
+
+    const selectedBrands = brand
+      .filter((b: TreeNode) => {
+        const selectedCategory = category.find(
+          (c: TreeNode) => c.id === b.category,
+        );
+        return !selectedCategory && b.selected;
+      })
+      .map((o: TreeNode) => 'All ' + o.name + ' ' + o?.parentNode?.name);
+
+    selectedBrands.length && selectedFilterOptions.push(selectedBrands);
+
+    // get selected model list
+
+    const selectedModels = model
+      .filter((m: TreeNode) => {
+        const selectedBrand = brand.find((b: TreeNode) => b.id === m.brand);
+        return !selectedBrand && m.selected;
+      })
+      .map((o: TreeNode) => 'All ' + o.name + ' devices');
+
+    selectedModels.length && selectedFilterOptions.push(selectedModels);
+
+    // get selected varient list
+    //Group by model
+
+    const selectedVarients = varient.filter((v: TreeNode) => {
+      const selectedModel = model.find((m: TreeNode) => m.id === v.model);
+      return !selectedModel && v.selected;
+    });
+
+    // grouping by model
+    let groupByModel = selectedVarients.reduce(
+      (filterList: any, item: any) => ({
+        ...filterList,
+        [item.parentNode.name]: [
+          ...(filterList[item.parentNode.name] || []),
+          item,
+        ],
+      }),
+      {},
+    );
+
+    const selectedVarientsByModel = Object.keys(groupByModel).map(key => {
+      const value = groupByModel[key];
+      const selectedVarientsForModel = value
+        .map((v: TreeNode) => v.name)
+        .join(', ');
+      return key + ' ' + selectedVarientsForModel;
+    });
+
+    selectedVarientsByModel.length &&
+      selectedFilterOptions.push(selectedVarientsByModel);
+
+    setSelectedFilters(selectedFilterOptions);
+  };
   return (
     <SafeAreaView
       style={{
@@ -95,8 +173,23 @@ const App = () => {
         flexDirection: 'column',
         position: 'relative',
       }}>
-      <View>
-        <ProductFilter data={filterMetadata} nodeView={NodeComponent} />
+      <View style={{flex: 9}}>
+        <ProductFilter
+          productList={filtereredList}
+          categoryList={productCategoryData}
+          nodeView={NodeComponent}
+          onSelection={(selectedNode: TreeNode[]) => {
+            formatFilterSelection(selectedNode);
+          }}
+        />
+      </View>
+      <View style={{flex: 1, padding: 5}}>
+        <Text style={{flex: 1, fontSize: 20, fontWeight: 'bold'}}>
+          Selected Filters
+        </Text>
+        <Text style={{flex: 1, fontSize: 10}}>
+          {selectedFilters.join(', ')}
+        </Text>
       </View>
     </SafeAreaView>
   );
